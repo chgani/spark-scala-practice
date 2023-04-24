fdcec86c97b264137963699c15cfb99865a96664
Ganesh <ganeshch.mech@gmail.com>
2023-04-24 21:41:28 +0530

    simple array problems

---
+++ arrayEasy/scalaArraySecondLargestDSAL.scala
@@ -0,0 +1,35 @@
+package arrayEasy
+
+object scalaArraySecondLargestDSAL {
+  def main(args:Array[String]):Unit={
+    val array=Array(23,34,56,32,23,87,98,56)
+    val secLargest1=secondlargestSimple1(array)
+    println(s"the second largest element (by method 1) in given array is $secLargest1")
+    val size=array.length
+    secondlargestSimple2(array,size)
+
+  }
+  def secondlargestSimple1(arr:Array[Int]):Int={
+    val arrDistinctSorted=arr.distinct.sortWith(_ > _)
+    //val len=arrDistinctSorted.length
+    arrDistinctSorted(1)
+  }
+  def secondlargestSimple2(arr:Array[Int],len:Int):Unit={
+    //val arrdistinct=arr.distinct
+    if(len<2) println("Invalid input")
+    var largest,second=Int.MinValue
+    for(i<- 0 until len-1){
+      if(arr(i)>largest) { //23,34,56,32,23,87,98,56
+        second = largest //minvalue,23,34
+        largest = arr(i) //23,34,56
+      }
+      else if(arr(i)>second && arr(i)!=largest){
+        second=arr(i)
+      }
+    }
+    println(s"the second largest element (by method 2) in given array is $second")
+  }
+
+
+}
+
---
+++ arrayEasy/scalaDSALpushZerosToEnd.scala
@@ -0,0 +1,31 @@
+package arrayEasy
+
+object scalaDSALpushZerosToEnd {
+  def main(args:Array[String]):Unit={
+    val array=Array(1,0,2,3,0,5,0,7,0,9,0,5,2,6,0,8,7,0,7,8,9)
+    val size=array.length
+    println("original array is:")
+    printArray(array,size)
+    pushzeros1(array,size)
+    println("\nmodified array is:")
+    printArray(array,size)
+  }
+
+  def pushzeros1(arr:Array[Int],len:Int)={
+    var count=0
+    for (i<- 0 until len){
+      if(arr(i)!=0){
+        arr(count)=arr(i)
+        count=count+1
+      }
+    }
+    for(i<- count until len){
+      arr(i)=0
+    }
+  }
+  def printArray(arr:Array[Int],len:Int)={
+    for(i<-0 until len){
+      print(arr(i)+" ")
+    }
+  }
+}
