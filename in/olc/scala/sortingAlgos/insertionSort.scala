d1316d39ec4dd2efd6bfbaf51944d392cb44f926
Ganesh <ganeshch.mech@gmail.com>
2023-05-10 20:32:05 +0530

    sorting algos

--- in/olc/scala/sortingAlgos/insertionSort.scala
+++ in/olc/scala/sortingAlgos/insertionSort.scala
@@ -6,18 +6,23 @@ object insertionSort {
     insertion(arr)
     println(arr.mkString(" "))
   }
-def insertion (arr:Array[Int]):Unit={
+def insertion (arr:Array[Int]):Array[Int]={
   for(i<-0 to arr.length-2){
     var j=i+1
-    while(j>0) {
-      if (arr(j) < arr(j - 1)) {
-        swap(arr, j, j - 1)
-        j -= 1
-      } else {
-        return
-      }
+//    while(j>0) {
+//      if (arr(j) < arr(j - 1)) {
+//        swap(arr, j, j - 1)
+//        j -= 1
+//      } else {
+//        return
+//      }
+//    }
+    while (j>0 && arr(j)<arr(j-1)){
+      swap(arr,j,j-1)
+      j-=1
     }
   }
+arr
 }
   def swap(arr:Array[Int],a1:Int,a2:Int)={
      val temp=arr(a1)
