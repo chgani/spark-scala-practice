3f3a92e6f092b8ed8f27deca71a7c813eeae7ac0
Ganesh <ganeshch.mech@gmail.com>
2023-05-15 21:41:44 +0530

    single linked list

---
+++ in/olc/scala/singleLinkedList/nodesCount.scala
@@ -0,0 +1,54 @@
+package in.olc.scala.singleLinkedList
+
+class node_2(var data:Int,var next:node_2)
+class nodesCount {
+  var head: node_2 = null
+
+  def insertEnd(new_data: Int) = {
+    val new_node = new node_2(new_data, null)
+    if (head == null) {
+      head = new_node
+    }
+    else {
+      var present: node_2 = head //we need to start from first and reach to end
+      while (present.next != null) {
+        present = present.next
+
+      }
+      present.next = new_node
+    }
+  }
+
+  def printList(): Unit = {
+    var temp = head
+    while (temp != null) {
+      println(temp.data + " ")
+      temp = temp.next
+    }
+  }
+  def count():Int={
+    var i:Int=0
+    var current=head
+    while(current!=null){
+      i=i+1
+      current=current.next
+    }
+    i
+  }
+}
+object count {
+  def main(args: Array[String]) = {
+    val ctLL = new nodesCount
+    ctLL.insertEnd(8)
+    ctLL.insertEnd(6)
+    ctLL.insertEnd(7)
+    ctLL.insertEnd(3)
+    ctLL.insertEnd(4)
+    ctLL.printList()
+    val result=ctLL.count()
+    println(s"number of nodes= $result")
+
+  }
+}
+
+
---
+++ in/olc/scala/singleLinkedList/reversalLinkedList.scala
@@ -0,0 +1,55 @@
+package in.olc.scala.singleLinkedList
+
+class node_e(var data:Int,var next:node_e)
+class reversalLinkedList {
+  var head: node_e = null
+
+  def insertEnd(new_data: Int) = {
+    val new_node = new node_e(new_data, null)
+    if (head == null) {
+      head = new_node
+    }
+    else {
+      var present: node_e = head
+      while (present.next != null) {
+        present = present.next
+      }
+      present.next = new_node
+    }
+  }
+
+  def display(): Unit = {
+    var temp = head
+    while (temp!= null) {
+      println(temp.data + " ")
+      temp = temp.next
+    }
+  }
+  def reverse()={
+    var prev:node_e=null
+    var next:node_e=null
+    var current=head //initially current=100
+    while(current!=null){ //head=100 ->10,200 (address=100) ->20,400(address=200) ->30,null(address=400) ->null
+      next=current.next //200;400
+      current.next=prev //null;100
+      prev=current //null;200
+      current=next //200;400
+    }
+    head=prev //30,200 (address=400)
+  }
+}
+  object reversal{
+    def main(args:Array[String])={
+      val revLL=new reversalLinkedList
+      revLL.insertEnd(5)
+      revLL.insertEnd(7)
+      revLL.insertEnd(6)
+      revLL.insertEnd(8)
+      revLL.display()
+      println("reversal")
+      revLL.reverse()
+      revLL.display()
+    }
+  }
+
+
